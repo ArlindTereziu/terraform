d620037361b93378cbb61b3675aa29be488e8e37
Arlind Tereziu <arlind.tereziu@gmail.com>
2023-10-02 21:20:31 +0200

    First commit

---
+++ backend.tf
@@ -0,0 +1,17 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+# The block below configures Terraform to use the 'remote' backend with Terraform Cloud.
+# For more information, see https://www.terraform.io/docs/backends/types/remote.html
+terraform {
+/*
+  cloud {
+    organization = "arlindtereziu"
+
+    workspaces {
+      name = "terraform"
+    }
+  }
+*/
+  required_version = ">= 1.1.2"
+}
