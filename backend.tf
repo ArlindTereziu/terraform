8bba4bc4be1392c3eb8f15dca163ad4622d56ac9
Arlind Tereziu <arlind.tereziu@gmail.com>
2023-10-02 21:36:30 +0200

    Add cloud block

--- backend.tf
+++ backend.tf
@@ -4,7 +4,6 @@
 # The block below configures Terraform to use the 'remote' backend with Terraform Cloud.
 # For more information, see https://www.terraform.io/docs/backends/types/remote.html
 terraform {
-/*
   cloud {
     organization = "arlindtereziu"
 
@@ -12,6 +11,5 @@ terraform {
       name = "terraform"
     }
   }
-*/
   required_version = ">= 1.1.2"
 }
