489adfba330a27a1df3b79ac2d505fc63512355a
Arlind Tereziu <arlind.tereziu@gmail.com>
2023-10-06 12:34:54 +0200

    Change to RandoPet

--- main.tf
+++ main.tf
@@ -1,9 +1,8 @@
-resource "random_id" "rg_name" {
+resource "random_pet" "rg_name" {
   prefix      = var.resource_group_name_prefix
-  byte_length = 1
 }
 
 resource "azurerm_resource_group" "rg" {
   location = var.resource_group_location
-  name     = random_id.rg_name.id
+  name     = random_pet.rg_name.id
 }
--- output.tf
+++ output.tf
@@ -1,3 +1,7 @@
 output "resource_group_name" {
   value = azurerm_resource_group.rg.name
 }
+
+output "random_pet_id" {
+  value = random_pet.rg_name.id
+}
