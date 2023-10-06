497afb358a7e62ec71bf39fc6492dbfcd905190b
Arlind Tereziu <arlind.tereziu@gmail.com>
2023-10-06 12:20:55 +0200

    Fix error on RG Name

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 resource "random_id" "rg_name" {
-  prefix = var.resource_group_name_prefix
+  prefix      = var.resource_group_name_prefix
   byte_length = 1
 }
 
 resource "azurerm_resource_group" "rg" {
   location = var.resource_group_location
-  name     = random_pet.rg_name.id
+  name     = random_id.rg_name.id
 }
